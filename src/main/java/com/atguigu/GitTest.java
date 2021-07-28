dffa0fc99b3d2ca55987b032e760ca97924643dd
wangli <1759098049@qq.com>
2021-07-28 17:32:25 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author shkstart
+ * @create 2021-07-28 17:28
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git test");
+    }
+}
