f9797e37b69845047f0c3debbd203143d5e3aa57
wangli <1759098049@qq.com>
2021-07-28 18:08:03 +0800

    push commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("git test master");
         System.out.println("git test hot-fix");
         System.out.println("git test hot-fix");
+        System.out.println("git test push");
     }
 }
